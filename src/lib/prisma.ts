3233e642822e83f9b22f339c56197b771a9bc104
Kibryant <arthurgustavon@gmail.com>
2024-10-11 09:27:13 -0300

    fix prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,8 +3,6 @@ import { Pool } from '@neondatabase/serverless'
 import { env } from './env'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
-export const runtime = 'edge'
-
 declare global {
   // eslint-disable-next-line no-var
   var cachedPrisma: PrismaClient
