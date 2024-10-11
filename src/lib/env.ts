8d9cbce0b682afcee6dc559f8d1e27b3c2209476
Kibryant <arthurgustavon@gmail.com>
2024-10-11 10:35:36 -0300

    try fix

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   SESSION_SECRET: z.string(),
   ADMIN_EMAIL: z.string().email(),
   ADMIN_PASSWORD: z.string().min(6),
+  NEXT_PUBLIC_BASE_URL: z.string().url(),
   POSTGRESQL_URL: z.string().url(),
 })
 
