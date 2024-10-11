c0ce2d8cd9919ef0f2af6fe2fdc912fcff7e3629
Kibryant <arthurgustavon@gmail.com>
2024-10-11 10:33:48 -0300

    try fix argon

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,14 +4,6 @@ const envSchema = z.object({
   SESSION_SECRET: z.string(),
   ADMIN_EMAIL: z.string().email(),
   ADMIN_PASSWORD: z.string().min(6),
-  NEXT_PUBLIC_FIREBASE_API_KEY: z.string(),
-  NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: z.string(),
-  NEXT_PUBLIC_FIREBASE_PROJECT_ID: z.string(),
-  NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: z.string(),
-  NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: z.string(),
-  NEXT_PUBLIC_FIREBASE_APP_ID: z.string(),
-  NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: z.string(),
-  NEXT_PUBLIC_BASE_URL: z.string().url(),
   POSTGRESQL_URL: z.string().url(),
 })
 
