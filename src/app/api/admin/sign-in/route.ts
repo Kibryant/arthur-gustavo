43753026d2d83fc3ebff0836d2e7472b6b00ef22
Kibryant <arthurgustavon@gmail.com>
2024-10-11 09:48:13 -0300

    try fix prisma

--- src/app/api/admin/sign-in/route.ts
+++ src/app/api/admin/sign-in/route.ts
@@ -1,10 +1,19 @@
 import { createSession } from '@/functions/create-session'
-import { prisma } from '@/lib/prisma'
 import { HttpStatusCode } from '@/types/http-status-code'
 import { type NextRequest, NextResponse } from 'next/server'
 import * as argon2 from 'argon2'
+import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { Pool } from '@neondatabase/serverless'
+import { env } from '@/lib/env'
+
+export const runtime = 'edge'
 
 export async function POST(req: NextRequest) {
+  const neon = new Pool({ connectionString: env.POSTGRESQL_URL })
+  const adapter = new PrismaNeon(neon)
+  const prisma = new PrismaClient({ adapter })
+
   const { email, password } = await req.json()
 
   const admin = await prisma.user.findUnique({
